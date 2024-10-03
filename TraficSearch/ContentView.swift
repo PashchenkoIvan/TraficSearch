385738f6729f0e3dd2f1d68c7b1d931d6067af70
PashchenkoIvan <ipaschenko09@gmail.com>
2024-10-03 15:10:05 +0300

    Initial Commit

---
+++ TraficSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TraficSearch
+//
+//  Created by Пащенко Иван on 03.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TraficSearch/TraficSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficSearchApp.swift
+//  TraficSearch
+//
+//  Created by Пащенко Иван on 03.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TraficSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
